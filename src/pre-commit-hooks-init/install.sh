632e07f445a62fdfeb78ed227882edb5034127f1
Samuel Kevetter <69881238+skevetter@users.noreply.github.com>
2025-09-20 18:29:14 +0000

    refactor: use search to find pre-commit path
    Signed-off-by: Samuel Kevetter <69881238+skevetter@users.noreply.github.com>

--- src/pre-commit-hooks-init/install.sh
+++ src/pre-commit-hooks-init/install.sh
@@ -6,21 +6,25 @@ set -eo pipefail
 
 NANOLAYER_VERSION="v0.5.6"
 
+init_precommit_hooks() {
+    workspace_dir=$(find /workspaces -mindepth 2 -maxdepth 2 -type f -name '.pre-commit-config.yaml' -print -quit | xargs -0 -r dirname)
+    if [ -n "$workspace_dir" ]; then
+        echo "cd ${workspace_dir} && pre-commit install --install-hooks"
+    else
+        echo "echo 'No .pre-commit-config.yaml found in any workspace folder. Skipping pre-commit hook installation.'"
+    fi
+}
+
 main() {
     echo "Ensuring nanolayer CLI (${NANOLAYER_VERSION}) is available"
     ensure_nanolayer nanolayer_location "${NANOLAYER_VERSION}"
 
-    echo "=========================================================="
-    printenv
-    echo "=========================================================="
-
-    echo "Initializing pre-commit hooks in workspace folder: ${WORKSPACEFOLDER}"
     # shellcheck disable=SC2154
     "${nanolayer_location}" \
         install \
         devcontainer-feature \
         "ghcr.io/devcontainers-extra/features/bash-command:1" \
-        --option command="cd ${WORKSPACEFOLDER} && pre-commit install --install-hooks"
+        --option command="$(init_precommit_hooks)"
 
     echo "Done!"
 }
