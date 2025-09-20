425cdf0e565a10355ef6c0a02039666aec8e2850
Samuel Kevetter <69881238+skevetter@users.noreply.github.com>
2025-09-20 20:30:44 +0000

    fix: update PRE_COMMIT_HOME export location from zshrc to zshenv
    Signed-off-by: Samuel Kevetter <69881238+skevetter@users.noreply.github.com>

--- test/pre-commit-cache/test.sh
+++ test/pre-commit-cache/test.sh
@@ -7,7 +7,6 @@ source dev-container-features-test-lib
 # The 'check' command comes from the dev-container-features-test-lib.
 check "verify /pre-commit-cache is present" test -d /pre-commit-cache
 check "PRE_COMMIT_HOME is set to /pre-commit-cache" test "$PRE_COMMIT_HOME" = "/pre-commit-cache"
-check "/etc/zsh/zshrc contains PRE_COMMIT_HOME" grep -q 'PRE_COMMIT_HOME=/pre-commit-cache' /etc/zsh/zshrc
 check "/etc/profile.d/pre_commit.sh contains PRE_COMMIT_HOME" grep -q 'PRE_COMMIT_HOME=/pre-commit-cache' /etc/profile.d/pre_commit.sh
 check "/etc/bash.bashrc contains PRE_COMMIT_HOME" grep -q 'PRE_COMMIT_HOME=/pre-commit-cache' /etc/bash.bashrc
 
