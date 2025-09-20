36c22d1b03adf03f28871ecb3a81867665bf3a96
Samuel Kevetter <69881238+skevetter@users.noreply.github.com>
2025-09-20 02:59:10 +0000

    refactor: pulumi installation script
    Signed-off-by: Samuel Kevetter <69881238+skevetter@users.noreply.github.com>

--- src/pulumi/install.sh
+++ src/pulumi/install.sh
@@ -10,23 +10,13 @@ main() {
     echo "Ensuring nanolayer CLI (${NANOLAYER_VERSION}) is available"
     ensure_nanolayer nanolayer_location "${NANOLAYER_VERSION}"
 
-    local arch_suffix
-    case "$(uname -m)" in
-        x86_64|amd64) arch_suffix="x64" ;;
-        aarch64|arm64) arch_suffix="arm64" ;;
-        *) error "Unsupported architecture: $(uname -m)"; return 1 ;;
-    esac
-
     echo "Installing Pulumi (version: ${VERSION})"
     # shellcheck disable=SC2154
     "${nanolayer_location}" \
         install \
         devcontainer-feature \
         "ghcr.io/devcontainers-extra/features/gh-release:1" \
-        --option repo="pulumi/pulumi" \
-        --option version="${VERSION}" \
-        --option assetRegex="^pulumi-.*-linux-${arch_suffix}\\.tar\\.gz$" \
-        --option binaryNames="pulumi"
+        --option repo="pulumi/pulumi" --option binaryNames="pulumi" --option version="${VERSION}"
 
     echo "Done!"
 }
