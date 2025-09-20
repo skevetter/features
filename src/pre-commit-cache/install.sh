6e7d20a877025bae9af4f8610ff376e1d0710c0f
Samuel Kevetter <69881238+skevetter@users.noreply.github.com>
2025-09-20 22:38:58 +0000

    chore: update pre-commit-cache to 1.0.4

--- src/pre-commit-cache/install.sh
+++ src/pre-commit-cache/install.sh
@@ -8,22 +8,10 @@ FEATURE_USER="${FEATURE_USER:-${_REMOTE_USER:-"vscode"}}"
 PRE_COMMIT_CACHE_DIR="/pre_commit_cache"
 NANOLAYER_VERSION="v0.5.6"
 
-setup_cache() {
+deploy() {
     mkdir -p "${PRE_COMMIT_CACHE_DIR}"
     chown "${FEATURE_USER}" "${PRE_COMMIT_CACHE_DIR}"
     chmod 700 "${PRE_COMMIT_CACHE_DIR}"
-}
-
-main() {
-    echo "Ensuring nanolayer CLI (${NANOLAYER_VERSION}) is available"
-    ensure_nanolayer nanolayer_location "${NANOLAYER_VERSION}"
-
-    # shellcheck disable=SC2154
-    "${nanolayer_location}" \
-        install \
-        devcontainer-feature \
-        "ghcr.io/devcontainers-extra/features/bash-command:1" \
-        --option command="$(setup_cache)"
 
     FILES=(/etc/profile.d/pre_commit_cache.sh /etc/zsh/zshenv /etc/bash.bashrc)
     for f in "${FILES[@]}"; do
@@ -37,6 +25,19 @@ export PRE_COMMIT_HOME=${PRE_COMMIT_CACHE_DIR}
 EOF
         fi
     done
+
+}
+
+main() {
+    echo "Ensuring nanolayer CLI (${NANOLAYER_VERSION}) is available"
+    ensure_nanolayer nanolayer_location "${NANOLAYER_VERSION}"
+
+    # shellcheck disable=SC2154
+    "${nanolayer_location}" \
+        install \
+        devcontainer-feature \
+        "ghcr.io/devcontainers-extra/features/bash-command:1" \
+        --option command="$(setup_cache)"
 
     echo "Done!"
 }
