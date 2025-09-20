b9709b8248888b7193dbce1d03bcbf4533257069
Samuel Kevetter <69881238+skevetter@users.noreply.github.com>
2025-09-20 18:07:14 +0000

    fix: add debugging to failing pre-commit feature
    Signed-off-by: Samuel Kevetter <69881238+skevetter@users.noreply.github.com>

--- src/pre-commit-hooks-init/install.sh
+++ src/pre-commit-hooks-init/install.sh
@@ -10,12 +10,16 @@ main() {
     echo "Ensuring nanolayer CLI (${NANOLAYER_VERSION}) is available"
     ensure_nanolayer nanolayer_location "${NANOLAYER_VERSION}"
 
+    echo "=========================================================="
+    printenv
+    echo "=========================================================="
+
     # shellcheck disable=SC2154
     "${nanolayer_location}" \
         install \
         devcontainer-feature \
         "ghcr.io/devcontainers-extra/features/bash-command:1" \
-        --option command="pre-commit install --install-hooks" \
+        --option command="git version && pre-commit install --install-hooks" \
         --option version="$VERSION"
 
     echo "Done!"
