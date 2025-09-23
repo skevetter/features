32a0cb31796812ca845df5f75dedd6de9cc66825
Samuel Kevetter <skevette@amazon.com>
2025-09-23 15:26:52 +0000

    fix: test for gpg-hack
    Signed-off-by: Samuel Kevetter <skevette@amazon.com>

--- test/gpg-hack/test.sh
+++ test/gpg-hack/test.sh
@@ -7,7 +7,6 @@ source dev-container-features-test-lib
 # The 'check' command comes from the dev-container-features-test-lib.
 check "verify /etc/gnupg/gpg.conf is present" test -f /etc/gnupg/gpg.conf
 check "verify /etc/gnupg/gpg-agent.conf is present" test -f /etc/gnupg/gpg-agent.conf
-check "verify GPG_TTY is set in bashrc" grep -q "export GPG_TTY=$(tty)" /etc/bash.bashrc
-check "verify GPG_TTY is set in zshrc" grep -q "export GPG_TTY=$(tty)" /etc/zsh/zshrc
+check "verify GPG_TTY is set in bashrc" grep -q "export GPG_TTY=\$(tty)" /etc/bash.bashrc
 
 reportResults
