f6e3c6cb754c923f246200ed586da512d181399f
Samuel Kevetter <skevette@amazon.com>
2025-09-28 21:31:26 +0000

    test: update tests
    Signed-off-by: Samuel Kevetter <skevette@amazon.com>

--- test/pre-commit-load-common-hooks/test.sh
+++ test/pre-commit-load-common-hooks/test.sh
@@ -5,8 +5,8 @@ set -e
 source dev-container-features-test-lib
 
 # The 'check' command comes from the dev-container-features-test-lib.
-check "verify /pre_commit_cache is present" test -d /pre_commit_cache
-check "verify pre-commit is installed" command -v pre-commit
-check "contents of /pre_commit_cache" ls -la /pre_commit_cache
+check "/pre_commit_cache is present" test -d /pre_commit_cache
+check "pre-commit is installed" command -v pre-commit
+check "owner of /pre_commit_cache" stat -c '%U' /pre_commit_cache
 
 reportResults
