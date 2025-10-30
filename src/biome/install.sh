68c3ce3d6ce6330d050dcbfefcae654c9749d53e
Samuel Kevetter <skevette@amazon.com>
2025-10-30 16:58:03 +0000

    feat(biome): use setup setup script from picolayer repository

--- src/biome/install.sh
+++ src/biome/install.sh
@@ -2,11 +2,33 @@
 
 set -eo pipefail
 
-. ./lib.sh
+setup_picolayer() {
+    PICOLAYER_BIN=$(curl -fsSL  https://raw.githubusercontent.com/skevetter/picolayer/refs/heads/main/install.sh | bash)
+}
+
+
+setup() {
+    if command -v apt-get >/dev/null 2>&1; then
+        apt-get update && apt-get install -y curl
+    elif command -v apk >/dev/null 2>&1; then
+        apk add --no-cache curl
+    else
+        echo "No supported package manager found" >&2; return 1
+    fi
+
+    setup_picolayer
+}
+
+cleanup() {
+    if command -v apt-get >/dev/null 2>&1; then
+        apt-get remove -y curl && apt-get autoremove -y && apt-get clean
+    elif command -v apk >/dev/null 2>&1; then
+        apk del curl
+    fi
+}
 
 main() {
-    echo "Ensuring picolayer CLI is available"
-    ensure_picolayer
+    setup
 
     echo "Installing Biome (version: ${VERSION})"
     # shellcheck disable=SC2154
@@ -16,6 +38,7 @@ main() {
         --option package='@biomejs/biome' --option version="$VERSION"
 
     echo "Done!"
+    cleanup
 }
 
 main "$@"
