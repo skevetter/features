9b3cdd53756c864c8157c5f6fb6c4b3e71f7de71
Samuel Kevetter <skevette@amazon.com>
2025-10-31 16:20:29 +0000

    feat(fzf): use picolayer install script
    Signed-off-by: Samuel Kevetter <skevette@amazon.com>

--- src/fzf/install.sh
+++ src/fzf/install.sh
@@ -2,13 +2,34 @@
 
 set -eo pipefail
 
-. ./lib.sh
+setup_picolayer() {
+    PICOLAYER_BIN=$(curl -fsSL  https://raw.githubusercontent.com/skevetter/picolayer/main/install.sh | bash)
+}
+
+
+setup() {
+    if command -v apt-get >/dev/null 2>&1; then
+        apt-get update && apt-get install -y curl
+    elif command -v apk >/dev/null 2>&1; then
+        apk add --no-cache curl
+    else
+        echo "No supported package manager found" >&2; return 1
+    fi
+
+    setup_picolayer
+}
+
+cleanup() {
+    if command -v apt-get >/dev/null 2>&1; then
+        apt-get remove -y curl && apt-get autoremove -y && apt-get clean
+    elif command -v apk >/dev/null 2>&1; then
+        apk del curl
+    fi
+}
 
 main() {
-    echo "Ensuring picolayer CLI is available"
-    ensure_picolayer
+    setup
 
-    echo "Installing Biome (version: ${VERSION})"
     "${PICOLAYER_BIN}" gh-release --owner junegunn --repo fzf --version "$VERSION"
 
     {
@@ -26,6 +47,7 @@ main() {
     fi
 
     echo "Done!"
+    cleanup
 }
 
 main "$@"
