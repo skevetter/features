a366af129107342bc680a185ed3ee1528165ae33
Samuel Kevetter <skevette@amazon.com>
2025-11-01 20:06:26 +0000

    refactor(fzf): use picolayer feature for install
    Signed-off-by: Samuel Kevetter <skevette@amazon.com>

--- src/fzf/install.sh
+++ src/fzf/install.sh
@@ -2,35 +2,8 @@
 
 set -eo pipefail
 
-setup_picolayer() {
-    PICOLAYER_BIN=$(curl -fsSL  https://raw.githubusercontent.com/skevetter/picolayer/main/install.sh | bash)
-}
-
-
-setup() {
-    if command -v apt-get >/dev/null 2>&1; then
-        apt-get update && apt-get install -y curl
-    elif command -v apk >/dev/null 2>&1; then
-        apk add --no-cache curl
-    else
-        echo "No supported package manager found" >&2; return 1
-    fi
-
-    setup_picolayer
-}
-
-cleanup() {
-    if command -v apt-get >/dev/null 2>&1; then
-        apt-get remove -y curl && apt-get autoremove -y && apt-get clean
-    elif command -v apk >/dev/null 2>&1; then
-        apk del curl
-    fi
-}
-
 main() {
-    setup
-
-    "${PICOLAYER_BIN}" gh-release --owner junegunn --repo fzf --version "$VERSION"
+    picolayer gh-release --owner junegunn --repo fzf --version "$VERSION"
 
     {
         echo ""
@@ -45,9 +18,6 @@ main() {
             echo "source <(fzf --zsh)"
         } >> /etc/zsh/zshrc
     fi
-
-    echo "Done!"
-    cleanup
 }
 
 main "$@"
