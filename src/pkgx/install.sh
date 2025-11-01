27a0aabc8c38b25f3b03e8a8431e265a24f7cda1
Samuel Kevetter <skevette@amazon.com>
2025-11-01 20:17:27 +0000

    refactor(pkgx): use picolayer feature for install
    Signed-off-by: Samuel Kevetter <skevette@amazon.com>

--- src/pkgx/install.sh
+++ src/pkgx/install.sh
@@ -2,22 +2,8 @@
 
 set -eo pipefail
 
-setup() {
-    if command -v apt-get >/dev/null 2>&1; then
-        apt-get update && apt-get install -y curl
-    elif command -v apk >/dev/null 2>&1; then
-        apk add --no-cache curl
-    else
-        echo "No supported package manager found" >&2; return 1
-    fi
-}
-
 main() {
-    setup
-
-    curl https://pkgx.sh | sh
-
-    echo "Done!"
+    picolayer gh-release --owner pkgxdev --repo pkgx --version "$VERSION" --verify-checksum --gpg-key https://dist.pkgx.dev/gpg-public.asc
 }
 
 main "$@"
