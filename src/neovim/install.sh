5f46d4b99c0fea254d472d9502c28305d9a451ef
Samuel Kevetter <skevette@amazon.com>
2025-11-01 20:11:13 +0000

    refactor(neovim): use picolayer feature for install
    Signed-off-by: Samuel Kevetter <skevette@amazon.com>

--- src/neovim/install.sh
+++ src/neovim/install.sh
@@ -2,33 +2,7 @@
 
 set -eo pipefail
 
-setup_picolayer() {
-    PICOLAYER_BIN=$(curl -fsSL  https://raw.githubusercontent.com/skevetter/picolayer/main/install.sh | bash)
-}
-
-setup() {
-    if command -v apt-get >/dev/null 2>&1; then
-        apt-get update && apt-get install -y curl
-    elif command -v apk >/dev/null 2>&1; then
-        apk add --no-cache curl
-    else
-        echo "No supported package manager found" >&2; return 1
-    fi
-
-    setup_picolayer
-}
-
-cleanup() {
-    if command -v apt-get >/dev/null 2>&1; then
-        apt-get remove -y curl && apt-get autoremove -y && apt-get clean
-    elif command -v apk >/dev/null 2>&1; then
-        apk del curl
-    fi
-}
-
 main() {
-    setup
-
     case "$(uname -s)" in
         Linux)
             os="linux"
@@ -58,10 +32,7 @@ main() {
 
     echo "Detected OS=${os} ARCH=${arch} -> Filter=${asset_regex}"
 
-    "${PICOLAYER_BIN}" gh-release --owner neovim --repo neovim --binary nvim --filter "${asset_regex}" --version "$VERSION"
-
-    echo "Done!"
-    cleanup
+    picolayer gh-release --owner neovim --repo neovim --binary nvim --filter "${asset_regex}" --version "$VERSION"
 }
 
 main "$@"
